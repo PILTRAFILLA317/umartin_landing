d47cb24ce46f133ff39242de24d7aebf0e4a14de
PILTRAFILLA317 <unaimartinm@gmail.com>
2025-09-26 18:32:21 +0200

    📈 Added analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,11 @@
 	import Particles from '../components/Particles/Particles.svelte';
 	import favicon from '$lib/assets/favicon.png';
 
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
+
 	let { children } = $props();
 </script>
 
@@ -14,4 +19,3 @@
 	{@render children?.()}
 	<Particles className="absolute opacity-40 inset-0 -z-10" refresh={true} />
 </div>
-
